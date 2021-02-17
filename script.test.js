d258179e59c655e0b8f3cf3cb42cfd6bbd9262a5
Danutzz8 <Marius.Prutean@outlook.com>
2021-02-17 17:09:33 +0200

    groupping the test

--- script.test.js
+++ script.test.js
@@ -8,13 +8,26 @@ dbMock = [
     'dogland.com'
 ]
 
-it('this is a silly test', () => {
-    expect('hello').toBe('hello')
-    googleSearch('dog', dbMock);
-});
-
-it('searching google', () => {
-    expect(googleSearch('testtest', dbMock)).toEqual(([]));
-    expect(googleSearch('dog', dbMock)).toEqual((['dog.com', 'dogland.com']));
-
+describe('googleSearch', () => {
+    it('this is a silly test', () => {
+        expect('hello').toBe('hello')
+        googleSearch('dog', dbMock);
+    });
+    
+    it('searching google', () => {
+        expect(googleSearch('testtest', dbMock)).toEqual(([]));
+        expect(googleSearch('dog', dbMock)).toEqual((['dog.com', 'dogland.com']));
+    
+    });
+    
+    it('work with undefined and null input', () => {
+        expect(googleSearch(undefined, dbMock)).toEqual(([]));
+        expect(googleSearch(null, dbMock)).toEqual(([]));
+    
+    });
+    
+    it('does not import more than 3 matches', () => {
+        expect(googleSearch('.com', dbMock).length).toEqual((3));
+    
+    })
 })
