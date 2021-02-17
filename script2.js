77b9cc4593db3c3b1c32563ed8c0983ac6c2b366
Danutzz8 <Marius.Prutean@outlook.com>
2021-02-17 17:40:02 +0200

    implementing fetch API and promises

---
+++ script2.js
@@ -0,0 +1,26 @@
+const fetch = require('node-fetch');
+
+const getPeoplePromise = fetch => {
+    return fetch('https://swapi.dev/api/people')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            return {
+                count: data.count,
+                result: data.result
+            }
+        });
+}
+
+const getPeople = async (fetch) => {
+    const getRequest = await fetch('https://swapi.dev/api/people')
+    const data = await getRequest.json()
+    console.log(data);
+    return {
+        count: data.count,
+        result: data.result
+    };
+}
+
+getPeople(fetch);
+
